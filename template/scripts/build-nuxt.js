fa85ab1c6c36b394d308af25b7b496150df5bcf9
Tarrask <damien.plumettaz@bluewin.ch>
2018-10-13 12:12:59 +0200

    prevent terminating process when analyze

--- template/scripts/build-nuxt.js
+++ template/scripts/build-nuxt.js
@@ -1,15 +1,32 @@
 
-const { Nuxt, Builder } = require('nuxt');
+const { Builder } = require('nuxt');
 
 module.exports = {
   friendlyName: 'Build',
   description: 'Build nuxt for production.',
-  inputs: {},
+  inputs: {
+    analyze: {
+      description: 'Run webpack analyse at the same time',
+      type: 'boolean',
+      defaultsTo: false
+    }
+  },
   fn: async function (inputs, exits) {
-    sails.log('Running custom shell script... (`build-nuxt`)');
-    await new Builder(sails.nuxt).build();
+    sails.log('Build nuxt for production...');
+
+		let nuxt = sails.nuxt;
+    let options = sails.nuxt.options;
+
+    if(inputs.analyze && typeof options.build.analyze !== 'object') {
+      options.build.analyze = true;
+    }
+
+    await new Builder(nuxt).build();
 
     // All done.
+    if(options.build.analyze === true || typeof options.build.analyze === 'object') {
+      return;
+    }
     return exits.success();
   }
 };
